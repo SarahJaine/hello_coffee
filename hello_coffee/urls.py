faeb739972cf7096058d2d1294760158e5202e41
SarahJaine <sarahjaine.sz@gmail.com>
2016-02-20 15:16:03 -0500

    add password files

--- hello_coffee/urls.py
+++ hello_coffee/urls.py
@@ -18,7 +18,25 @@ from django.contrib import admin
 from collection import views
 from django.views.generic import TemplateView
 
+from django.contrib.auth.views import (
+    password_reset,
+    password_reset_done, 
+    password_reset_confirm, 
+    password_reset_complete,
+)
+
 urlpatterns = [
+    url(r'^accounts/password/reset/$', password_reset,
+        {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
+    url(r'^accounts/password/reset/done/$', 
+        password_reset_done,
+        {'texmplate_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/ (?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm,
+        {'template_name': 'registration/password_reset_confirm.html'}, 
+        name="password_reset_confirm"),
+    url(r'^accounts/password/done/$', password_reset_complete,
+        {'template_name': 'registration/password_reset_complete.html'},
+        name="password_reset_complete"),
 	url(r'^$', views.index, name='home'),
 	url(r'^about/$',
 		TemplateView.as_view(template_name='about.html'), 
